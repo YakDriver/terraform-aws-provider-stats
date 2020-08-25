910b5f41c4ca63ad3dcbbdc9c7f991ab67c7c9f0
Dirk Avery <dirk.avery@gmail.com>
2020-08-25 15:29:02 -0400

    Add initial functionality

--- get_stats.sh
+++ get_stats.sh
@@ -8,6 +8,10 @@ declare -a descriptions
 declare -a all_files
 declare -a tests
 
+descriptions+=( "Everything (For the Most Part)" )
+all_files+=( "*.go" )
+tests+=( "*_test.go" )
+
 descriptions+=( "Resources" )
 all_files+=( "resource_aws_*.go" )
 tests+=( "resource_aws_*_test.go" )
@@ -38,11 +42,15 @@ for i in "${!descriptions[@]}"; do
   ((non_test_files=${all_count}-${test_count}))
   ((non_test_lines=${all_lines}-${test_lines}))
 
-  printf "## %s\n\n" "${descriptions[$i]}" >> "${readmeFile}"
-  printf "All Files:%d\n" "${all_count}"  >> "${readmeFile}"
-  printf "Non-Test Files:%d\n" "${non_test_files}"  >> "${readmeFile}"
-  printf "Test Files:%d\n" "${test_count}"  >> "${readmeFile}"
-  printf "All Lines of Code:%d\n" "${all_lines}"  >> "${readmeFile}"
-  printf "Non-Test Lines:%d\n" "${non_test_lines}"  >> "${readmeFile}"
-  printf "Test Lines:%d\n" "${test_lines}"  >> "${readmeFile}"
+  printf "\n\n## %s\n\n" "${descriptions[$i]}" >> "${readmeFile}"
+
+  printf "|  %s  |  %s  |\n" "Stat" "Count" >> "${readmeFile}"
+  printf "| ------------- | -------------: |\n" >> "${readmeFile}"
+
+  printf "|  %s  |  %'.f  |\n" "All Files" "${all_count}"  >> "${readmeFile}"
+  printf "|  %s  |  %'.f  |\n" "Non-Test Files" "${non_test_files}"  >> "${readmeFile}"
+  printf "|  %s  |  %'.f  |\n" "Test Files" "${test_count}"  >> "${readmeFile}"
+  printf "|  %s  |  %'.f  |\n" "Lines of Code" "${all_lines}"  >> "${readmeFile}"
+  printf "|  %s  |  %'.f  |\n" "Non-Test Lines" "${non_test_lines}"  >> "${readmeFile}"
+  printf "|  %s  |  %'.f  |\n\n" "Test Lines" "${test_lines}"  >> "${readmeFile}"
 done
